2d79381f6df21a2e109ebdd4f8a9000106d45a98
develop-man1 <smailhit2003@gmail.com>
2026-03-20 03:46:50 +0500

    -2

--- app/models/permission.py
+++ app/models/permission.py
@@ -7,6 +7,6 @@ class Permission(Base):
     
     __tablename__ = "permissions"
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     resource = Column(String, nullable=False, unique=True, index=True)
     action = Column(String, nullable=False, index=True)
